b5d5d773c29cf61e2ddfe69d729cff951d50b7c2
adiy55 <adiyafe55@gmail.com>
2023-03-23 09:55:43 +0200

    Fix signal handler, update functions

--- myshell.c
+++ myshell.c
@@ -4,55 +4,71 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <setjmp.h>
 
 #define MAX_LINE_LEN 1024
 #define WHITESPACE " \t\n\r"
 
+// init global variables for shell
+char command[MAX_LINE_LEN + 1];
+char last_command[MAX_LINE_LEN + 1] = ""; // initialize last_command as empty string
+int last_command_flag = 0;
+char *outfile = NULL, *token;
+int i, fd, amper, redirect, retid, status, piping;
+char *argv1[10], *argv2[10];
+int argc1, redirect_fd, append;
+char prompt[MAX_LINE_LEN + 1] = "hello";
+sigjmp_buf jmpbuf;
 
-int parse_command(char **argv, char *command) {
-    /* parse command line */
-    int i = 0;
-    char *token = strtok(command, WHITESPACE);
-    while (token != NULL) {
-        argv[i++] = token;
-        token = strtok(NULL, WHITESPACE);
-    }
-    argv[i] = NULL;
-    return i;
+void handleOutputRedirect();
+
+int handleShellCommands();
+
+// signal handler for Ctrl-C
+void sigint_handler(int sig) {
+    write(STDOUT_FILENO, "\nYou typed Control-C!\n", 22);
+    siglongjmp(jmpbuf, 1);
 }
 
-void args_handler(char **argv, int argc, char **outfile, int *redirect, int *amper, char *prompt) {
-    if (!strcmp(argv[argc - 1], "&")) {
-        *amper = 1;
-        argv[argc - 1] = NULL;
+void parseCommand() {
+    /* parse command line */
+    i = 0;
+    token = strtok(command, WHITESPACE);
+    while (token != NULL) {
+        argv1[i++] = token;
+        token = strtok(NULL, WHITESPACE);
+        if (token && !strcmp(token, "|")) {
+            piping = 1;
+            break;
+        }
     }
-    else if (argc > 1 && !strcmp(argv[argc - 2], ">")) {
-        *redirect = 1;
-        argv[argc - 2] = NULL;
-        *outfile = argv[argc - 1];
-    }
-    // Change prompt
-    else if (argc == 3 && strcmp(argv[0], "prompt") == 0 && strcmp(argv[1], "=") == 0) {
-        strcpy(prompt, argv[2]);
+    argv1[i] = NULL;
+    argc1 = i;
+
+    /* Is command empty */
+    if (argv1[0] == NULL) { return; }
+
+    /* Does command contain pipe */
+    if (piping) {
+        i = 0;
+        while (token != NULL) {
+            token = strtok(NULL, WHITESPACE);
+            argv2[i++] = token;
+        }
+        argv2[i] = NULL;
     }
 }
 
 int main() {
-    // init variables for shell
-    char command[MAX_LINE_LEN + 1];
-    char last_command[MAX_LINE_LEN + 1] = ""; // initialize last_command as empty string
-    int last_command_flag = 0;
-    char *token;
-    char *outfile = NULL;
-    int i, fd, amper, redirect, retid, status;
-    char *argv[10];
-    char prompt[MAX_LINE_LEN + 1] = "hello";
-
     memset(command, 0, MAX_LINE_LEN + 1);
 
-    while (1) {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = sigint_handler;
+    sigaction(SIGINT, &sa, NULL);
+    sigsetjmp(jmpbuf, 1);
 
-        memset(command, 0, MAX_LINE_LEN + 1);
+    while (1) {
 
         printf("%s: ", prompt);
 
@@ -60,34 +76,42 @@ int main() {
             break;
         }
 
-        command[strlen(command) - 1] = '\0'; // replace \n with \0
-        redirect = amper = 0;
-
         // get command line arguments
-        int argc = parse_command(argv, command);
+        parseCommand();
 
         /* Is command empty */
-        if (argv[0] == NULL) {
+        if (argv1[0] == NULL) {
             continue;
         }
-
-        if (strcmp(argv[0], "quit") == 0) {
+        if (strcmp(argv1[0], "quit") == 0) {
             break;
         }
 
-        args_handler(argv, argc, &outfile, &redirect, &amper, prompt);
+        if (!strcmp(argv1[argc1 - 1], "&")) {
+            amper = 1;
+            argv1[argc1 - 1] = NULL;
+        }
+
+        handleOutputRedirect();
+
+        if (handleShellCommands()) {
+            continue;
+        }
 
         /* for commands not part of the shell command language */
         if (fork() == 0) {
-            /* redirection of IO ? */
+            /* redirection of IO */
             if (redirect) {
-                fd = creat(outfile, 0660);
-                close(STDOUT_FILENO);
+                if (append) {
+                    fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0660);
+                } else {
+                    fd = creat(outfile, 0660);
+                }
+                close(redirect_fd);
                 dup(fd);
                 close(fd);
-                /* stdout is now redirected */
             }
-            execvp(argv[0], argv);
+            execvp(argv1[0], argv1);
         }
         /* parent continues here */
         if (amper == 0) {
@@ -96,4 +120,47 @@ int main() {
     }
 
     return 0;
+}
+
+int handleShellCommands() {
+    // 2. Change prompt
+    if (argc1 == 3 && strcmp(argv1[0], "prompt") == 0 && strcmp(argv1[1], "=") == 0) {
+        strcpy(prompt, argv1[2]);
+        return 1;
+    }
+
+    // 3. Echo command
+    if (strcmp(argv1[0], "echo") == 0) {
+        for (int j = 1; j < argc1; ++j) {
+            if (strcmp(argv1[j], "$?") == 0) {
+                printf("%d ", WEXITSTATUS(status)); // prints the status of the last command executed
+            } else {
+                printf("%s ", argv1[j]);
+            }
+        }
+        printf("\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+void handleOutputRedirect() {
+    redirect = amper = append = 0;
+    redirect_fd = STDOUT_FILENO;
+    int flag = 0;
+    if (argc1 > 1) {
+        if (!strcmp(argv1[argc1 - 2], ">")) {
+            redirect = flag = 1;
+        } else if (!strcmp(argv1[argc1 - 2], ">>")) {
+            redirect = append = flag = 1;
+        } else if (!strcmp(argv1[argc1 - 2], "2>")) { // redirect stderr
+            redirect = flag = 1;
+            redirect_fd = STDERR_FILENO;
+        }
+    }
+    if (flag) {
+        argv1[argc1 - 2] = NULL;
+        outfile = argv1[argc1 - 1];
+    }
 }
